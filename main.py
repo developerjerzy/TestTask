9036f7cea77b16303e0efe80c2fe556b6ab5ada5
jurasach <jurasach@mail.ru>
2021-11-10 15:22:17 +0300

    change class DrawingPlots

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-patients_df = pd.read_json('pandas_json.txt')
-patients_df.plot()
+#class DrawingPlots:
+ #   """Класс для соддания графиков переданных объектов"""
+
+  #  def __init__(self,path_pandas_json):
+  #      """Конструктор для передачи параметров """
+   #     self.path_pandas_json = path_pandas_json
+
+  #  def draw_plots(self):
+parametrs_df = pd.read_json("pandas_json.txt")
+
+
+
+
+
+
+#if __name__ == "__main__":
+ #   drawing_plots_one = DrawingPlots("pandas_json.txt")
+  #  drawing_plots_one.draw_plots()
+
+
 
 
 
